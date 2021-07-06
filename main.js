e9304a4f5650dfdcaea60b4a1fe4210feba62e2d
parth <parthmodhvadiya15@gmail.com>
2021-07-06 17:27:30 +0530

    navbar added

---
+++ main.js
@@ -0,0 +1,22 @@
+$(".open-popup-link").magnificPopup({
+    type: "inline",
+    midClick: true,
+    mainClass: "mfp-fade",
+  });
+ 
+  $(window).resize(function () {
+    /*If browser resized, check width again */
+    if ($(window).width() < 769) {
+      $("nav ul").addClass("mfp-hide");
+    } else {
+      $("nav ul").removeClass("mfp-hide");
+    }
+  });
+  $(window).resize(function () {
+    /*If browser resized, check width again */
+    if ($(window).width() < 769) {
+      $(".open-poppup-link").addClass("mfp-hide");
+    } else {
+      $(".open-poppup-link").removeClass("mfp-hide");
+    }
+  });
